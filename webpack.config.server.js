08afae8898d3d91c6784c4acac039144ff9f064d
unknown <dorultanianosgyorgy@gmail.com>
2020-01-08 11:51:42 +0000

    let heroku bind the port

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -42,7 +42,8 @@ const serverConfig = {
 								"DB_NAME": JSON.stringify(process.env.DB_NAME),
 								"AWS_ACCESS_KEY_ID": JSON.stringify(process.env.AWS_ACCESS_KEY_ID),
 								"AWS_SECRET_ACCESS_KEY": JSON.stringify(process.env.AWS_SECRET_ACCESS_KEY),
-								"AWS_BUCKET": JSON.stringify(process.env.AWS_BUCKET)
+								"AWS_BUCKET": JSON.stringify(process.env.AWS_BUCKET),
+								"PORT": JSON.stringify(process.env.PORT)
             }
         }),
         new webpack.NoEmitOnErrorsPlugin(),
