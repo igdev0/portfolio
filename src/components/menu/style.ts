9473fb754aacd6ac622a050dba638f775eca4976
dorultan <dorultanianosgyorgy@gmail.com>
2021-12-12 22:25:21 +0200

    fix: type issue

--- src/components/menu/style.ts
+++ src/components/menu/style.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 import vars from "../../styles/vars";
-import {AppThemeType} from "../../state/app";
 import {lighten} from "polished";
 
-export const MenuWrapper = styled.div<AppThemeType>`
+export const MenuWrapper = styled.div`
   margin: 0;
   padding: 1em;
   height: 100%;
