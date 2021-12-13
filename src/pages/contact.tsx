dd521de1d82d35413a4120f3206d8ed4c8ad00f0
dorultan <dorultanianosgyorgy@gmail.com>
2021-12-13 14:21:01 +0200

    feat: two new pages

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import Page from "../components/page/page";
+
+const META = {
+  title: "Dorultan Ianos | contact",
+}
+export default function Contact() {
+    return (
+        <Page meta={META} pageContentTitle={`Contact me`}>
+
+        </Page>
+    );
+}
---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import Page from "../components/page/page";
+
+const META = {
+  title: "Dorultan Ianos | projects",
+}
+export default function Projects() {
+    return (
+        <Page meta={META} pageContentTitle={`Projects`}>
+
+        </Page>
+    );
+}
