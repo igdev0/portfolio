a1ee8e521c39cd9fff6b85b6e0dd7731b2ee3ff6
dorultan <dorultanianosgyorgy@gmail.com>
2021-12-19 15:30:54 +0200

    wip: request cv

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,11 @@
+import {AtRule} from "csstype";
+import Page = AtRule.Page;
+
+export default function PrivacyPolicy() {
+    return (
+        <Page>
+            
+            </Page>
+
+    )
+}
