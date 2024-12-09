24203d88b19f09bbb3c1634ae79d8b8099803145
igdev <dorultanianos@gmail.com>
2024-12-09 13:02:35 +0200

    chore: clean up old code

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,30 +1,11 @@
 import {AppRootStyled} from './app.styled.ts';
-import ExperienceSection from '../experience/experience.tsx';
-import EducationSection from '../education/education.tsx';
 import {useRef} from 'react';
-import {useScroll, useTransform} from 'framer-motion';
-import Navbar from '../navbar/navbar.tsx';
-import TechStackScreen from '../tech-stack/tech-stack.tsx';
-import Header from '../header/header.tsx';
 
 
 function App() {
   const ref = useRef<HTMLDivElement>(null);
-  const {scrollYProgress} = useScroll({
-    target: ref,
-    container: ref
-  });
-  const hidden = useTransform(() => {
-    return scrollYProgress.get() > 0.1 ? '0%' : '-100%';
-  });
-
   return (
       <AppRootStyled ref={ref}>
-        <Navbar hidden={hidden}/>
-        <Header/>
-        <TechStackScreen/>
-        <ExperienceSection/>
-        <EducationSection/>
       </AppRootStyled>
   );
 }
