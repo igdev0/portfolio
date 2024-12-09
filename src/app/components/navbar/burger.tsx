40815eb4ddb9aed39505393d7d852ec97b668b77
igdev <dorultanianos@gmail.com>
2024-12-09 16:40:42 +0200

    feat(navbar): add change cb to burger

--- src/app/components/navbar/burger.tsx
+++ src/app/components/navbar/burger.tsx
@@ -1,9 +1,17 @@
+"use client";
 import styles from './burger.module.scss';
+import {ChangeEvent} from 'react';
 
-export default function Burger() {
+interface Props {
+  onChange: (changed: boolean) => void
+}
+export default function Burger(props: Props) {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    props.onChange(event.currentTarget.checked)
+  }
   return (
       <label htmlFor="check" className={styles.burger}>
-        <input className={styles.burger__trigger} type="checkbox" id="check"/>
+        <input className={styles.burger__trigger} type="checkbox" id="check" onChange={handleChange}/>
         <span className={styles.burger__slice}></span>
         <span className={styles.burger__slice}></span>
         <span className={styles.burger__slice}></span>
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,10 +1,13 @@
+"use client";
 import styles from './navbar.module.scss';
 import Burger from '@/app/components/navbar/burger';
 
 export default function Navbar() {
+  const handleBurgerChange = (changed: boolean) => {
+  }
   return (
       <div className={styles.navbar}>
-        <Burger/>
+        <Burger onChange={handleBurgerChange}/>
       </div>
   )
 }
