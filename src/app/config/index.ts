e498ae420d38ce61c8ba3d81bccc432676d878db
igdev <dorultanianos@gmail.com>
2024-12-10 20:02:51 +0200

    feaat(config): add email, github and linkedin url to the config

---
+++ src/app/config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  githubUrl: "https://github.com/igdev0",
+  linkedinUrl: "https://www.linkedin.com/in/ianos-dorultan-364235143/",
+  email: "dorultanianos@gmail.com",
+};
+
+export default config;
