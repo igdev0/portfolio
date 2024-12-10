b5bb174b1fb89d2e75b09879909607732c4b5bd6
igdev <dorultanianos@gmail.com>
2024-12-10 21:14:04 +0200

    feat(navbar): implement links

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -3,6 +3,7 @@ import styles from './navbar.module.scss';
 import Burger from '@/app/components/navbar/burger';
 import {Github, LinkedIn, Mail} from '@/app/components/navbar/icons';
 import ThemeButton from '@/app/components/navbar/theme-button';
+import config from '@/app/config';
 
 export default function Navbar() {
 
@@ -12,9 +13,15 @@ export default function Navbar() {
       <div className={styles.navbar}>
         <Burger onChange={handleBurgerChange}/>
         <div className={styles.navbar__socialbox}>
-          <Mail/>
-          <Github/>
-          <LinkedIn/>
+          <a href={`mailto:${config.email}`} rel="noreferrer">
+            <Mail/>
+          </a>
+          <a href={config.githubUrl} target="_blank" rel="noopener">
+            <Github/>
+          </a>
+          <a href={config.linkedinUrl} target="_blank" rel="noopener">
+            <LinkedIn/>
+          </a>
           <ThemeButton/>
         </div>
       </div>
