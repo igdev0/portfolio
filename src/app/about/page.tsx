e2ba3b09d3aadb25b39476bdad8bbe04e7c916a1
igdev <dorultanianos@gmail.com>
2024-12-14 22:38:43 +0200

    feat(about): init about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import styles from "./styles.module.scss";
+import Navbar from '@/app/components/navbar/navbar';
+
+export default function About() {
+  return (
+        <div className={styles.page}>
+          <Navbar/>
+        </div>
+  );
+}
