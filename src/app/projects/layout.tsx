3e5abc83a494d75b38b4fc4764dad47f62961df0
igdev <dorultanianos@gmail.com>
2025-01-01 11:58:04 +0200

    chore: update metadata of projects page

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import {PropsWithChildren} from 'react';
 
 export const metadata: Metadata = {
-  title: "Dorultan Ianos | Projects",
-  description: "...[todo]",
+  title: "IGDev's projects",
+  description: "This page contains a list of projects developed by Ianos Dorultan",
 };
 
 export default function Layout(props: PropsWithChildren) {
