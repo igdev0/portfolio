dc6c23edabf968ad8a12e94ddd98122f54259234
igdev <dorultanianos@gmail.com>
2025-01-09 22:48:24 +0200

    feat(navbar): add bg scroll animation

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -13,6 +13,7 @@ import {usePathname} from 'next/navigation';
 
 export default function Navbar() {
   const burgerRef = useRef<HTMLLabelElement>(null);
+  const navbarRef =useRef<HTMLDivElement>(null);
   const pathname = usePathname();
   const {menuOpen, toggleMenu} = useContext(AppContext);
   const [offset, setOffset] = useState(0);
@@ -45,37 +46,60 @@ export default function Navbar() {
     toggleMenu(false);
   }, [pathname, toggleMenu]);
 
+  useEffect(() => {
+    let previousScroll = 0;
+    window.addEventListener("scroll", () => {
+      if(!navbarRef.current) {
+        return;
+      }
+      if (previousScroll < window.scrollY) {
+        navbarRef.current.style.background = "transparent";
+        navbarRef.current.style.transform = "translateY(-100%)";
+      } else {
+        navbarRef.current.style.background = "var(--background)";
+        navbarRef.current.style.transform = "translateY(0)";
+      }
+
+      if(window.scrollY < navbarRef.current.clientHeight) {
+        navbarRef.current.style.background = "transparent";
+      }
+      previousScroll = window.scrollY
+    })
+  }, [navbarRef]);
+
   return (
       <>
         <Menu open={menuOpen} offset={offset}/>
-        <div className={styles.navbar}>
-          <div className={styles.navbar__links}>
-            <Navlink href="/">
-              Root
-            </Navlink>
-            <Navlink href="/about-me">
-              About me
-            </Navlink>
-            <Navlink href="/projects">
-              Projects
-            </Navlink>
-            <Navlink href="/contact">
-              Contact
-            </Navlink>
-          </div>
-          <Burger onChange={handleBurgerChange} ref={burgerRef}/>
-          <div className={styles.navbar__socialbox}>
-            <a href={`mailto:${config.email}`} rel="noreferrer">
-              <Mail/>
-            </a>
-            <a href={config.githubUrl} target="_blank" rel="noopener">
-              <Github/>
-            </a>
-            <a href={config.linkedinUrl} target="_blank" rel="noopener">
-              <LinkedIn/>
-            </a>
-            <div className={styles.navbar__social__separator}/>
-            <ThemeButton/>
+        <div className={styles.navbar} ref={navbarRef}>
+          <div className={styles.navbar__container}>
+            <div className={styles.navbar__links}>
+              <Navlink href="/">
+                Root
+              </Navlink>
+              <Navlink href="/about-me">
+                About me
+              </Navlink>
+              <Navlink href="/projects">
+                Projects
+              </Navlink>
+              <Navlink href="/contact">
+                Contact
+              </Navlink>
+            </div>
+            <Burger onChange={handleBurgerChange} ref={burgerRef}/>
+            <div className={styles.navbar__socialbox}>
+              <a href={`mailto:${config.email}`} rel="noreferrer">
+                <Mail/>
+              </a>
+              <a href={config.githubUrl} target="_blank" rel="noopener">
+                <Github/>
+              </a>
+              <a href={config.linkedinUrl} target="_blank" rel="noopener">
+                <LinkedIn/>
+              </a>
+              <div className={styles.navbar__social__separator}/>
+              <ThemeButton/>
+            </div>
           </div>
         </div>
       </>
