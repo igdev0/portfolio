b43a6215540b66d3358a52e345486af8519d5c7c
igdev <dorultanianos@gmail.com>
2025-01-27 21:09:15 +0200

    fix: burger state

--- src/app/components/navbar/burger.tsx
+++ src/app/components/navbar/burger.tsx
@@ -1,6 +1,6 @@
 "use client";
 import styles from './burger.module.scss';
-import {ChangeEvent, ForwardedRef, forwardRef} from 'react';
+import {ChangeEvent, ForwardedRef, forwardRef, useState} from 'react';
 
 interface Props {
   onChange: (changed: boolean) => void
@@ -9,12 +9,14 @@ interface Props {
 
 
 function Burger(props: Props, ref: ForwardedRef<HTMLLabelElement>) {
+  const [value, setValue] = useState(false);
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     props.onChange(event.currentTarget.checked)
+    setValue(v => !v);
   }
   return (
       <label htmlFor="check" className={styles.burger} ref={ref}>
-        <input className={styles.burger__trigger} type="checkbox" id="check" onChange={handleChange}/>
+        <input className={styles.burger__trigger} type="checkbox" id="check" onChange={handleChange} checked={value}/>
         <span className={styles.burger__slice}></span>
         <span className={styles.burger__slice}></span>
         <span className={styles.burger__slice}></span>
