fa26a24a44f1babb003bd9cbdde09cf010ce49bd
igdev <dorultanianos@gmail.com>
2026-01-30 16:09:10 +0200

    remove contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import styles from './page.module.scss';
 import {FormEvent, useState} from 'react';
-import Button from '@/app/components/button/button';
-import config from '@/app/config';
 import {ReCaptchaProvider} from 'next-recaptcha-v3';
 
 interface ContactForm {
@@ -71,25 +69,7 @@ export default function Contact() {
       <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY??""}>
       <div className={styles.contact}>
         <div className={styles.contact__container}>
-          <h1>Contact me</h1>
-          <p>Use this form to send me a message, I will try to reach out as soon as I can, otherwise reach out on <a href={config.linkedinUrl} target="_blank" rel="noopener">Linkedin</a> or <a href={config.x} target="_blank" rel="noopener">x (former twitter)</a>. </p>
-          <form onSubmit={handleSubmit} className={styles.contact__form}>
-            <fieldset className={styles.contact__form__field}>
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" placeholder="john.doe@domain.com" value={data.email}
-                     onChange={handleInputChange}/>
-              {errors.email && <div className={styles.contact__form__field__error}>{errors.email}</div>}
-            </fieldset>
-            <fieldset className={styles.contact__form__field}>
-              <label htmlFor="message">Message</label>
-              <textarea value={data.message} name="message" placeholder="Type a message ..." maxLength={150}
-                        onChange={handleInputChange}/>
-              {errors.message && <div className={styles.contact__form__field__error}>{errors.message}</div>}
-            </fieldset>
-            {apiError && <p className={styles.contact__form__error}>{apiError}</p>}
-            {successMsg && <p className={styles.contact__form__success}>{successMsg}</p>}
-            <Button asLink={false} variant="filled">Submit</Button>
-          </form>
+          <h1>Available Soon.</h1>
         </div>
       </div>
       </ReCaptchaProvider>
