3fa1663a05b149e059dd7e78a4a91ae60d5e21bc
igdev <dorultanianos@gmail.com>
2026-04-04 08:39:16 +0300

    clean up

--- src/components/ui/container/index.tsx
+++
@@ -1,9 +0,0 @@
-import {PropsWithChildren} from 'react';
-
-export default function Container({children}: PropsWithChildren) {
-  return (
-      <div className="max-w-[1100] mx-auto px-4">
-        {children}
-      </div>
-  );
-}
