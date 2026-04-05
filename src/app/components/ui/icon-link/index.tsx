4d66148f876f00b469c7b1ec87747c0700e67ff6
igdev <dorultanianos@gmail.com>
2026-04-05 17:56:08 +0300

    fix: navbar layout

--- src/app/components/ui/icon-link/index.tsx
+++ src/app/components/ui/icon-link/index.tsx
@@ -1,15 +1,23 @@
 import Link from 'next/dist/client/link';
 import Icon, {IconTypes} from '@/app/components/ui/icon';
 import "../icon-button/index.css";
+import clsx from 'clsx';
 
 export interface IconLinkProps {
   icon: IconTypes;
   href: string;
+  className?: string;
 }
 
 export default function IconLink(props: IconLinkProps) {
+  let cls = clsx("icon-button");
+
+  if (props.className) {
+    cls += " " + (props.className ?? "");
+  }
+
   return (
-      <Link href={props.href}>
+      <Link className={cls} href={props.href}>
         <Icon type={props.icon}/>
       </Link>
   );
