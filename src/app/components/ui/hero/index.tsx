af7511876b5ebf5af9203859352b0af9274a276f
igdev <dorultanianos@gmail.com>
2026-04-05 15:06:40 +0300

    feat hero column

--- src/app/components/ui/hero/index.tsx
+++ src/app/components/ui/hero/index.tsx
@@ -3,14 +3,15 @@ import LinkButton from '@/app/components/ui/link-button';
 import clsx from 'clsx';
 import Statement from '@/app/components/ui/statement';
 import {Image} from 'next/dist/client/image-component';
+import "./index.css";
 
 const corners = clsx(`h-10 w-10 m-4 border-r-12 border-b-12 border-(--bg-surface)`);
 export default function Hero() {
   return (
-      <header className={clsx('h-[800] flex flex-col px-6')}>
+      <header className="hero">
         <div
-            className="max-w-275 border-x border-(--semigrid) mx-auto gap-4 w-full px-9 pt-30 grid sm:grid-cols-2 items-center sm:justify-between">
-          <div className="sm:order-1">
+            className="hero__layout">
+          <div className="hero__column">
             <div className="mb-2" dangerouslySetInnerHTML={{__html: hero.tag.html}}/>
             <h1 className="text-5xl font-bold mb-6">{hero.title.text}</h1>
             <Statement className="mb-6">
@@ -26,7 +27,7 @@ export default function Hero() {
             </div>
           </div>
           <div
-              className="flex items-center self-end sm:w-full justify-normal w-max sm:justify-end sm:order-2 relative sm:max-w-[400] ml-auto md:mr-17 z-0">
+              className="hero__column">
 
             <Image draggable={false} src={hero.card.image.src} width={354} height={393}
                    alt={hero.card.image.alt}/>
