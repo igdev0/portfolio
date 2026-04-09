a8f2ebe4722bcff75c0d8ec8660128c060d95e5d
igdev <dorultanianos@gmail.com>
2026-04-09 10:50:14 +0300

    refactor hero to use box instead of div

--- src/app/components/ui/hero/index.tsx
+++ src/app/components/ui/hero/index.tsx
@@ -7,14 +7,15 @@ import "./index.css";
 import Container from '@/app/components/lib/container';
 import AppText from '@/app/components/lib/text';
 import Html from '@/app/components/lib/html';
+import Box from '@/app/components/lib/box';
 
 const corners = clsx(`absolute h-1/9 w-1/9 m-4 border-r-12 border-b-12 border-(--bg-surface)`);
 
 export default function Hero() {
   return (
-      <header className="hero">
+      <Box className="hero" as="header">
         <Container className="hero__layout">
-          <div className="hero__column">
+          <Box className="hero__column">
             <Html className="mb-3 relative">
               {hero.tag.html}
             </Html>
@@ -25,27 +26,27 @@ export default function Hero() {
             <Statement className="mb-6 max-w-96">
                 {hero.statement.text}
             </Statement>
-            <div className="flex gap-4">
+            <Box className="flex gap-4">
               <LinkButton variant="outline" href="#about">
                 {hero.cta.first.text}
               </LinkButton>
               <LinkButton variant="solid" href="#about">
                 {hero.cta.second.text}
               </LinkButton>
-            </div>
-          </div>
-          <div
+            </Box>
+          </Box>
+          <Box
               className="hero__column hero__column--image">
             <Image draggable={false} src={hero.card.image.src} loading="eager" width={354} height={393}
                    alt={hero.card.image.alt}/>
-            <div className="bg-(--bg-surface) w-3/5 h-3/5 absolute -z-1 top-0 left-0 right-0 bottom-0 m-auto"/>
-            <div className={clsx(corners, 'rotate-180 top-3 left-0')}/>
-            <div
+            <Box className="bg-(--bg-surface) w-3/5 h-3/5 absolute -z-1 top-0 left-0 right-0 bottom-0 m-auto"/>
+            <Box className={clsx(corners, 'rotate-180 top-3 left-0')}/>
+            <Box
                 className={clsx(corners, 'rotate-90 left-0 bottom-0')}/>
-            <div className={clsx(corners, 'right-0 bottom-0')}/>
-            <div className={clsx(corners, '-rotate-90 right-0 top-3')}/>
-          </div>
+            <Box className={clsx(corners, 'right-0 bottom-0')}/>
+            <Box className={clsx(corners, '-rotate-90 right-0 top-3')}/>
+          </Box>
         </Container>
-      </header>
+      </Box>
   );
 }
