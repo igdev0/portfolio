e9cbfac243fcab5b94ea6c4791f09f38674979f2
igdev <dorultanianos@gmail.com>
2026-04-09 10:53:51 +0300

    wip profile

--- src/app/components/ui/profile/index.tsx
+++ src/app/components/ui/profile/index.tsx
@@ -1,18 +1,19 @@
 import Comment from '@/app/components/lib/comment';
 import Container from '@/app/components/lib/container';
 import AppText from '@/app/components/lib/text';
+import Box from '@/app/components/lib/box';
 
 export default function Profile() {
   return (
       <Container>
-        <section className="profile pt-40">
+        <Box as="section" className="profile pt-40">
           <Comment>
             // Profile
           </Comment>
           <AppText size="4xl" weight="bold">
             About
           </AppText>
-        </section>
+        </Box>
       </Container>
   );
 }
