e7be5a7f54ae5d596da0f9b347ded1051257b173
igdev <dorultanianos@gmail.com>
2026-04-10 18:40:55 +0300

    wip expertise section

--- src/app/components/lib/expertise/index.tsx
+++ src/app/components/lib/expertise/index.tsx
@@ -2,6 +2,8 @@ import Comment from '@/app/components/lib/comment';
 import Container from '@/app/components/lib/container';
 import Statement from '@/app/components/lib/statement';
 import "./index.css";
+import {PropsWithChildren} from 'react';
+import LinkButton from '@/app/components/lib/link-button';
 
 
 interface ExternalLink {
@@ -11,26 +13,53 @@ interface ExternalLink {
 
 export interface ExperienceProps {
   title: string;
-  location: string;
-  contractType: string;
-  remote: boolean;
-  startDate: string;
-  endDate: string;
+  tags: string[];
   responsibilities: string[];
   links: ExternalLink[];
 }
 
+
+function Tag(props: PropsWithChildren) {
+  return (
+      <div className="px-2 py-1 rounded-sm border border-default-500 bg-default">
+        {props.children}
+      </div>
+  );
+}
 
 function Experience(props: ExperienceProps) {
   return (
       <div className="experience">
         <div className="timeline">
           <div className="timeline__head"/>
-          <div className="timeline__bar"/>
         </div>
         <div>
           <h3 className="text-2xl font-bold">{props.title}</h3>
-          <div style={{height: '500px'}}/>
+          <div className="flex flex-wrap gap-4 mt-4 relative">
+            {
+              props.tags.map((tag, index) => (
+                  <Tag key={index}>
+                    {tag}
+                  </Tag>
+              ))
+            }
+          </div>
+          <ul className="flex flex-col gap-2 font-medium my-10">
+            {
+              props.responsibilities?.map((responsibility, index) => (
+                  <li key={index}><span className="text-accent-500">{'>'}</span> {responsibility}</li>
+              ))
+            }
+          </ul>
+          <div className="flex flex-wrap gap-3">
+            {
+              props.links.map((link, index) => (
+                  <LinkButton href={link.href} variant="secondary" icon="github">
+                    {link.text}
+                  </LinkButton>
+              ))
+            }
+          </div>
         </div>
       </div>
   );
@@ -42,10 +71,23 @@ const contents = {
   experiences: [
     {
       title: "[Open-Source Engineer]",
-      contractType: "Full-Time",
-      remote: true,
-      startDate: "December 2022",
-      endDate: "December 2024",
+      tags: ["Input output Global", "Full-Time", "Remote", "December 2022 → April 2024"],
+      responsibilities: ['Led the front-end development of the web tools.', 'Led the front-end development of the web tools.', 'Led the front-end development of the web tools.'],
+      links: [
+        {
+          href: "",
+          text: "STAR App",
+        },
+        {
+          href: "",
+          text: "Teva MS App",
+        }
+      ],
+    },
+    {
+      title: "[Frontend Engineer]",
+      tags: ["Finer Vision", "Full-Time", "Hybrid", "December 2020 → February 2022"],
+      responsibilities: ['Led the front-end development of the web tools.', 'Led the front-end development of the web tools.', 'Led the front-end development of the web tools.'],
       links: [
         {
           href: "",
@@ -65,14 +107,17 @@ export default function Expertise() {
       <Container className="pt-40">
         <Comment>{contents.comment}</Comment>
         <h1 className="text-4xl font-bold mt-3">{contents.title}</h1>
-        <Statement>
+        <Statement className="mb-6">
           I got more than 5 years of experience.
         </Statement>
-        {
-          contents.experiences.map((experience, index) => (
-              <Experience key={index + 1} {...experience}/>
-          ))
-        }
+        <div className="relative my-6">
+          <div className="border-l-3 border-gray-200 border-dashed h-full absolute left-0 translate-x-2.5 -z-1"/>
+          {
+            contents.experiences.map((experience, index) => (
+                <Experience key={index + 1} {...experience}/>
+            ))
+          }
+        </div>
       </Container>
   );
 }
--- src/app/components/lib/link-button/index.tsx
+++ src/app/components/lib/link-button/index.tsx
@@ -2,16 +2,19 @@ import "../button/index.css";
 import AppLink, {AppLinkProps} from '@/app/components/lib/link';
 import {buttonVariants} from '@/app/components/lib/button';
 import {VariantProps} from 'class-variance-authority';
+import Icon, {IconNames} from '@/app/components/lib/icon';
 
 interface LinkButtonProps extends AppLinkProps, VariantProps<typeof buttonVariants> {
+  icon?: IconNames;
 }
 
 export default function LinkButton(props: LinkButtonProps) {
-  const {href, children, className = '', ...variants} = props;
+  const {href, children, icon, className = '', ...variants} = props;
   return (
       <AppLink
           className={`${buttonVariants(variants)} ${className}`}
           href={href}>
+        {icon && <Icon name={icon}/>}
         {children}
       </AppLink>
   );
