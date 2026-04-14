864c522354314ce15cb006ff7919ecb85812d6ce
igdev <dorultanianos@gmail.com>
2026-04-14 09:59:50 +0300

    make docker images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.0.126']
+  output: "standalone",
+  allowedDevOrigins: ['0.0.0.0'],
 };
 
 export default nextConfig;
