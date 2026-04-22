e248775caef383475048730e6acbbc62779dd982
igdev <dorultanianos@gmail.com>
2026-04-22 08:39:59 +0300

    remove package

--- apps/page/src/theme/global/spaces.css.ts
+++
@@ -1,9 +0,0 @@
-import {createGlobalTheme} from '@vanilla-extract/css';
-import {layers} from './layers.css.ts';
-
-export const spaces = createGlobalTheme(":root", {
-  "@layer": layers.utilities,
-  sm: "1rem",
-  md: "2rem",
-  lg: "3rem",
-})
