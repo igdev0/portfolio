017f8ff204735bd0b70027e85bebccc2281e2ce6
igdev <dorultanianos@gmail.com>
2026-04-27 13:32:57 +0300

    wip v2

--- src/components/lib/tech-stack-v2/index.tsx
+++ src/components/lib/tech-stack-v2/index.tsx
@@ -1,24 +1,33 @@
 import {stack} from '@/components/lib/tech-stack-v2/const';
-import {useLayoutEffect, useMemo, useRef, useState} from 'react';
+import {useLayoutEffect, useMemo, useState} from 'react';
 import Button from '@/components/lib/button';
 import "./index.css";
 import Container from '@/components/lib/container';
-import {createDraggable, createScope, Draggable, Scope, spring, utils} from 'animejs';
+import {createDraggable, Draggable, JSAnimation, spring, utils} from 'animejs';
 
 export interface TechStackProps {
   data: typeof stack;
 }
 
+interface FrameRef {
+  target: HTMLElement | null;
+  animation: JSAnimation | null;
+  key: string;
+  isNext: boolean;
+  distance: number;
+  offset: number;
+  scale: number;
+  z: number;
+  index: number;
+}
+
 export type StackKey = keyof typeof stack;
 const threshold = 60;
 export default function TechStackV2(props: TechStackProps) {
-  const root = useRef<HTMLDivElement>(null);
-  const scope = useRef<Scope>(null);
-
   const stackKeys = [...Object.keys(props.data)];
   const [active, setActive] = useState(0);
 
-  const frames = useMemo(() => {
+  const frames = useMemo<FrameRef[]>(() =>{
     return stackKeys.map((key, index) => {
       let delta = index - active;
       const total = stackKeys.length;
@@ -30,16 +39,18 @@ export default function TechStackV2(props: TechStackProps) {
       const offset = 0.13;
 
       return {
+        target: null,
+        animation: null,
         key: key as StackKey,
-        isActive: index === active,
+        isNext: false,
         distance,
-        offsetValue: delta * 30,
+        offset: delta * 30,
         z: total - distance,
         scale: 1 - offset * distance,
         index,
       };
-    });
-  }, [active, stackKeys]);
+    })
+  }, [active]);
 
   const safeIndex = (next: number) => {
     if (next < 0) return stackKeys.length - 1;
@@ -50,14 +61,10 @@ export default function TechStackV2(props: TechStackProps) {
   const onUpdate = (draggable: Draggable) => {
     const distance = calcDistance(draggable.x, draggable.y);
 
-    const next = calcNext(draggable);
-
-    const scale = Math.max(0.7, 1 - distance / 300);
-
     utils.set(draggable.$target, {
       x: draggable.x,
       y: draggable.y,
-      scale,
+      scale: Math.max(0.7, 1 - distance / 300),
     });
   };
 
@@ -83,38 +90,25 @@ export default function TechStackV2(props: TechStackProps) {
   };
 
   useLayoutEffect(() => {
-    scope.current = createScope({root}).add((self) => {
-      const cards = self?.root.querySelectorAll(".stack-card");
-      if (!cards) {
-        throw new Error(`No cards found`);
-      }
+    for (const {index: i, target: card} of frames) {
+      if (!card) continue;
+      utils.set(card, {y: frames[i].offset, scale: frames[i].scale, z: frames[i].z});
+      const cardWidth = card.clientWidth;
+      const cardHeight = card.clientHeight;
 
-      let i = 0;
-      for (const card of cards) {
-
-        utils.set(card, {y: frames[i].offsetValue, scale: frames[i].scale, z: frames[i].z});
-        const cardWidth = card.clientWidth;
-        const cardHeight = card.clientHeight;
-
-        const draggable = createDraggable(card, {
-          container: [-cardWidth, cardHeight, cardHeight, -cardWidth],
-          releaseEase: spring({bounce: .2}),
-          snap: [0,0,0,0],
-          onUpdate,
-          onRelease,
-        });
-        draggable.setY(frames[i].offsetValue);
-        i++;
-      }
-
-    });
-    return () => scope.current!.revert();
-  }, [root, active]);
-
+      createDraggable(card, {
+        container: [-cardWidth, cardHeight, cardHeight, -cardWidth],
+        releaseEase: spring({bounce: .2}),
+        snap: [0, 0, 0, 0],
+        onUpdate,
+        onRelease,
+      });
+    }
+  }, [frames]);
 
   return (
       <Container>
-        <div className="tech-stack-v2" ref={root}>
+        <div className="tech-stack-v2">
           <div className="stack-controllers">
             {frames.map((item, index) => (
                 <Button onClick={() => setActive(index)} variant="secondary" key={item.key}>{item.key}</Button>))}
@@ -123,10 +117,16 @@ export default function TechStackV2(props: TechStackProps) {
           <div className="stack-cards">
             {
               frames.map((frame, index) => (
-                  <div key={frame.key}
-                       data-order={index}
-                       style={{transform: `scale(${frame.scale}) translateZ(${frame.z}) translateY(${frame.offsetValue}px)`}}
-                       className="stack-card">{frame.key}</div>)
+                      <div key={frame.key}
+                           ref={(ref) => {
+                             frame.target = ref;
+                           }}
+                           data-order={index}
+                           style={{transform: `scale(${frame.scale}) translateZ(${frame.z}) translateY(${frame.offset}px)`}}
+                           className="stack-card">
+                        {frame.key}
+                      </div>
+                  )
               )
             }
           </div>
