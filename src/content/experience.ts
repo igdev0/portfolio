7e9f9ca2eb06a915e346112239ce81638d78b305
igdev <dorultanianos@gmail.com>
2026-05-05 12:57:02 +0300

    wip: experience

--- src/content/experience.ts
+++ src/content/experience.ts
@@ -4,24 +4,37 @@ export const experience = {
   statement: "I got more than 5 years of experience",
   experiences: [
     {
-      title: "[Open-Source Engineer]",
+      title: "[Frontend Engineer]",
       tags: ["Input output Global", "Full-Time", "Remote", "December 2022 → April 2024"],
-      responsibilities: ['Led the front-end development of the web tools.', 'Led the front-end development of the web tools.', 'Led the front-end development of the web tools.'],
+      responsibilities: [
+        'Led the front-end development of the web tools',
+        'Contributed to each step in the process of development, from ideating to real-thing agent infrastructure provisioning tool',
+        'Developed reusable react component library',
+        'Worked closely with the UI designer to develop the best UI/UX possible',
+        'Maintained the legacy software'
+      ],
       links: [
         {
-          href: "",
-          text: "STAR App",
+          href: "https://hyperledger-identus.github.io/docs/",
+          text: "Identus",
         },
-        {
-          href: "",
-          text: "Teva MS App",
-        }
       ],
     },
     {
-      title: "[Frontend Engineer]",
+      title: "[Full-Stack Developer]",
       tags: ["Finer Vision", "Full-Time", "Hybrid", "December 2020 → February 2022"],
-      responsibilities: ['Led the front-end development of the web tools.', 'Led the front-end development of the web tools.', 'Led the front-end development of the web tools.'],
+      responsibilities: [
+        "Contributed to a few native apps using React native and Expo",
+        "Collaborated with some clients directly, to ease the development process and help the team",
+        "Contributed to resolving complex app data migration issues",
+        "Transformed designs into pixel-perfect responsive and functional code",
+        "Automated content integration into apps using Google APIs",
+        "Delivered timely ahead of project deadline most of the time",
+        "Supported team mates fixing bugs & helping them meet the deadline",
+        "Equipped virtual machines with the necessary software for web services",
+        "Maintained legacy projects and fixed bunch of bugs",
+        "Developed numerous bespoke designed emails",
+      ],
       links: [
         {
           href: "",
@@ -40,12 +53,20 @@ export const experience = {
       responsibilities: ['Led the front-end development of the web tools.', 'Led the front-end development of the web tools.', 'Led the front-end development of the web tools.'],
       links: [
         {
-          href: "",
+          href: "https://apps.apple.com/gb/app/star-the-symptom-tracker/id1560476806?platform=iphone",
           text: "STAR App",
         },
         {
-          href: "",
+          href: "https://play.google.com/store/apps/details?id=com.finervision.RFD",
+          text: "RFD App",
+        },
+        {
+          href: "https://play.google.com/store/apps/details?id=com.elevenLondon.MSConnections",
           text: "Teva MS App",
+        },
+        {
+          href: 'https://bluespherebio.com/',
+          text: "Bluespherebio",
         }
       ],
 
