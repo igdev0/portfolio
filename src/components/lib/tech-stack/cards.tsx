0bca31a533f1ba4f27048a6b75075c4874d581ff
igdev <dorultanianos@gmail.com>
2026-05-05 10:34:22 +0300

    finalize refactoring

--- src/components/lib/tech-stack/cards.tsx
+++ src/components/lib/tech-stack/cards.tsx
@@ -1,12 +1,75 @@
-import {useContext} from 'react';
+import {useContext, useLayoutEffect, useRef} from 'react';
 import {TechStackContext} from '@/components/lib/tech-stack/context';
 import Statement from '@/components/lib/statement';
+import {animate, createDraggable, createScope, Draggable, Scope} from 'animejs';
+import {calcNext} from '@/components/lib/tech-stack/utils';
+
+const threshold = 60;
 
 export default function TechStackCards() {
-  const {frames, cards} = useContext(TechStackContext);
+  const {frames, cards, activeRef, active, setActive, dragOffsetRef, keys} = useContext(TechStackContext);
+  const root = useRef<HTMLDivElement>(null);
+  const scope = useRef<Scope>(null);
+
+
+  const onRelease = (draggable: Draggable) => {
+    const nextIndex = calcNext(draggable, activeRef, threshold, keys);
+
+    dragOffsetRef.current.x = 0;
+    dragOffsetRef.current.y = 0;
+
+    setActive(nextIndex);
+  };
+
+  const stackCards = () => {
+    for (const frame of frames) {
+      animate(cards.current[frame.i], {
+        translateZ: frame.scale,
+        translateY: frame.offset,
+        translateX: 0,
+        scaleZ: frame.z,
+        duration: 200,
+      });
+    }
+    scope.current?.refresh();
+  };
+
+  const onAfterResize = () => {
+    stackCards();
+  };
+
+  const onUpdate = (draggable: Draggable) => {
+    dragOffsetRef.current.x = draggable.x;
+    dragOffsetRef.current.y = draggable.y;
+  };
+
+  useLayoutEffect(() => {
+    if (scope.current) {
+      scope.current.revert();
+    }
+    scope.current = createScope({root}).add(() => {
+      for (const frame of frames) {
+        createDraggable(cards.current[frame.i], {
+          snap: [0, 0, 0, 0],
+          x: activeRef.current === frame.i,
+          y: activeRef.current === frame.i,
+          onAfterResize,
+          onRelease,
+          onUpdate,
+        });
+      }
+    });
+
+  }, []);
+
+  useLayoutEffect(() => {
+    activeRef.current = active;
+    stackCards();
+  }, [active]);
+
   return (
 
-      <div className="stack-cards">
+      <div className="stack-cards" ref={root}>
         {
           frames.map((frame, index) => (
                   <div
--- src/components/lib/tech-stack/index.tsx
+++ src/components/lib/tech-stack/index.tsx
@@ -1,8 +1,6 @@
-import {useContext, useLayoutEffect, useRef} from 'react';
+import {useContext, useLayoutEffect} from 'react';
 import "./index.css";
-import {animate, createDraggable, createScope, Draggable, Scope} from 'animejs';
 import useResizeObserver from '@/hooks/use-resize-observer';
-import {calcNext} from '@/components/lib/tech-stack/utils';
 import {stack} from '@/content/profile';
 import {TechStackContext} from '@/components/lib/tech-stack/context';
 import TechStackControllers from '@/components/lib/tech-stack/controllers';
@@ -11,64 +9,15 @@ import TechStackOverlay from '@/components/lib/tech-stack/overlay';
 
 
 export type StackKey = keyof typeof stack;
-const threshold = 60;
 
 export default function TechStack() {
-  const {active, setActive, keys, activeRef, dragOffsetRef, drawsRef, draws, calculateDraws, cards, frames} = useContext(TechStackContext);
-  const [addRef, , root] = useResizeObserver();
-  const scope = useRef<Scope>(null);
+  const {
+    drawsRef,
+    draws,
+    calculateDraws,
+  } = useContext(TechStackContext);
+  const [addRef] = useResizeObserver();
 
-
-  const onRelease = (draggable: Draggable) => {
-    const nextIndex = calcNext(draggable, activeRef, threshold, keys);
-
-    // reset drag offset so line snaps back
-    dragOffsetRef.current.x = 0;
-    dragOffsetRef.current.y = 0;
-
-    setActive(nextIndex);
-  };
-
-  const stackCards = () => {
-    for (const frame of frames) {
-      animate(cards.current[frame.i], {
-        translateZ: frame.scale,
-        translateY: frame.offset,
-        translateX: 0,
-        scaleZ: frame.z,
-        duration: 200,
-      });
-    }
-    scope.current?.refresh();
-  };
-
-  const onAfterResize = () => {
-    stackCards();
-  };
-
-  const onUpdate = (draggable: Draggable) => {
-    dragOffsetRef.current.x = draggable.x;
-    dragOffsetRef.current.y = draggable.y;
-  };
-
-  useLayoutEffect(() => {
-    if (scope.current) {
-      scope.current.revert();
-    }
-    scope.current = createScope({root}).add(() => {
-      for (const frame of frames) {
-        createDraggable(cards.current[frame.i], {
-          snap: [0, 0, 0, 0],
-          x: activeRef.current === frame.i,
-          y: activeRef.current === frame.i,
-          onAfterResize,
-          onRelease,
-          onUpdate,
-        });
-      }
-    });
-
-  }, []);
 
   useLayoutEffect(() => {
     drawsRef.current = draws;
@@ -77,11 +26,6 @@ export default function TechStack() {
   useLayoutEffect(() => {
     calculateDraws();
   }, []);
-
-  useLayoutEffect(() => {
-    activeRef.current = active;
-    stackCards();
-  }, [active]);
 
   return (
       <div className="tech-stack"
