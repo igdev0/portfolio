6d16dbc03323a3e895db7f9c77d8e049f269bcb4
igdev <dorultanianos@gmail.com>
2026-05-05 08:25:52 +0300

    make hero accept data prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import Hero from '@/components/ui/hero';
 import Profile from '@/components/ui/profile';
 import Expertise from '@/components/lib/expertise';
 import menu from '@/content/menu';
+import hero from '@/content/hero';
 
 export default function LandingPage() {
   return (
       <>
           <Nav data={menu}/>
-          <Hero/>
+          <Hero data={hero}/>
           <Profile/>
           <Expertise/>
       </>
--- src/components/ui/hero/index.tsx
+++ src/components/ui/hero/index.tsx
@@ -11,38 +11,42 @@ import Comment from '@/components/lib/comment';
 
 const corners = clsx(`absolute h-1/9 w-1/9 m-4 border-r-12 border-b-12 border-(--bg-surface)`);
 
-export default function Hero() {
+interface HeroProps {
+  data: typeof hero;
+}
 
+export default function Hero(props: HeroProps) {
+  const {data} = props;
   return (
       <Box className="hero" as="header">
         <Container className="hero__layout">
           <Box className="hero__column">
             <Comment className="comment mb-3">
-              {hero.tag.value}
+              {data.tag.value}
             </Comment>
             <h1
-                className="text-5xl mb-6 font-bold">{hero.title.value}
+                className="text-5xl mb-6 font-bold">{data.title.value}
             </h1>
             <Statement className="mb-6 max-w-96">
-              {hero.statement.value}
+              {data.statement.value}
             </Statement>
             <Box className="flex gap-4">
               <LinkButton variant="outline" href="#about">
-                {hero.cta.first.text}
+                {data.cta.first.text}
               </LinkButton>
               <LinkButton variant="solid" href="#about">
-                {hero.cta.second.text}
+                {data.cta.second.text}
               </LinkButton>
             </Box>
           </Box>
           <Box
               className="hero__column hero__column--image">
             <Image draggable={false}
-                   src={hero.card.image.src}
+                   src={data.card.image.src}
                    loading="eager"
                    width={354}
                    height={393}
-                   alt={hero.card.image.alt}/>
+                   alt={data.card.image.alt}/>
             <Box className="bg-(--bg-surface) w-3/5 h-3/5 absolute -z-1 top-0 left-0 right-0 bottom-0 m-auto"/>
             <Box className={clsx(corners, 'rotate-180 top-3 left-0')}/>
             <Box
