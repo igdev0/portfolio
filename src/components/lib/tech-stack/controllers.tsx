3618bfda9e69bfd0468931dd61dfa0ec9bd2d843
igdev <dorultanianos@gmail.com>
2026-05-05 11:34:18 +0300

    fix: cards appereance and add controller icon

--- src/components/lib/tech-stack/controllers.tsx
+++ src/components/lib/tech-stack/controllers.tsx
@@ -3,7 +3,7 @@ import {TechStackContext} from '@/components/lib/tech-stack/context';
 import Button from '@/components/lib/button';
 
 export default function TechStackControllers() {
-  const {frames, active, controllers, setActive} = useContext(TechStackContext);
+  const {frames, active, controllers, data, setActive} = useContext(TechStackContext);
 
   const handleButtonClick = (index: number) => {
     return () => {
@@ -13,12 +13,16 @@ export default function TechStackControllers() {
   return (
 
       <div className="stack-controllers">
-        {frames.map((item, index) => (
-            <Button active={active === index} disabled={active === index} ref={(el) => {
-              if (el) {
-                controllers.current[index] = el;
-              }
-            }} onClick={handleButtonClick(index)} variant="secondary" key={item.key}>{item.key}</Button>))}
+        {frames.map((item, index) => {
+          const content = data[item.key as keyof typeof data];
+          return (
+              <Button icon={content.icon} active={active === index} disabled={active === index} ref={(el) => {
+                if (el) {
+                  controllers.current[index] = el;
+                }
+              }} onClick={handleButtonClick(index)} variant="secondary" key={item.key}>{item.key}</Button>
+          );
+        })}
       </div>
-  )
+  );
 }
