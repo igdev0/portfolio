0d6b323cde321e1466bec058f935b087c5455419
igdev <dorultanianos@gmail.com>
2026-05-12 11:42:48 +0300

    remove default title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,6 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <title>Default Page title</title>
       </head>
       <body className={(`${lexend.className}`)}>
       {process.env.NODE_ENV === "production" && <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID ?? "not set"}/>}
