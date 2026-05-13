19a89ee670bab17c50cfc2c870e4616f64c4ec18
igdev <dorultanianos@gmail.com>
2026-05-13 20:29:29 +0300

    refactor basic message into its own feature

--- src/components/lib/collaborate/index.tsx
+++ src/components/lib/collaborate/index.tsx
@@ -5,28 +5,14 @@ import {CollaborateType} from '@/content/collaborate';
 import "./index.css";
 import LinkButton from '@/components/lib/link-button';
 import {IconNames} from '@/components/lib/icon';
-import {MessageCircle, XIcon} from 'lucide-react';
-import Button from '@/components/lib/button';
-import {SubmitEventHandler, useState} from 'react';
-import {notifyDiscord} from '@/app/actions';
+import BasicMessage from '@/features/basic-message';
 
 interface CollaborateProps {
   data: CollaborateType;
 }
 
 export default function Collaborate(props: CollaborateProps) {
-  const [message, setMessage] = useState('');
-  const [success, setSuccess] = useState(false);
   const {data} = props;
-
-  const handleSubmit: SubmitEventHandler = async (event) => {
-    event.preventDefault();
-    if (message.length) {
-      await notifyDiscord(message);
-      setMessage("");
-      setSuccess(true);
-    }
-  };
 
   return (
       <Container className="pt-40 pb-4" id="collaborate">
@@ -38,38 +24,7 @@ export default function Collaborate(props: CollaborateProps) {
           {data.statement}
         </Statement>
         <div className="max-w-150 gap-6 w-full">
-          <form onSubmit={handleSubmit} className="mt-2">
-            <label className="flex flex-col gap-4 w-full h-full" htmlFor="message">
-              <div className="flex flex-wrap-reverse w-full gap-4 items-baseline justify-between">
-                <span className="flex gap-4 mb-2">
-                <MessageCircle/><span>Write me a message:</span>
-                </span>
-                <LinkButton size="small" className="w-fit" external href={props.data.social.calendar.href}
-                            icon="calendar">
-                  {props.data.social.calendar.text}
-                </LinkButton>
-              </div>
-              <textarea className="border border-(--semigrid) p-2 rounded-sm h-30" name="message"
-                        value={message}
-                        onChange={(event) => setMessage(event.currentTarget.value)}
-                        placeholder="Say Hello ..."/>
-              <Button className="self-end" variant="solid-light" type="submit" icon="send"
-                      aspect="square"
-                      disabled={message.length === 0}
-                      iconPosition="right"></Button>
-            </label>
-          </form>
-          {
-              success && (
-                  <div
-                      className="relative rounded-sm bg-(--bg-surface-2) p-2 mb-6 flex items-center border border-(--semigrid) w-fit">
-                    Message received. Looking forward to reading it.
-                    <button className="cursor-pointer pl-2" onClick={() => setSuccess(false)}>
-                      <XIcon/>
-                    </button>
-                  </div>
-              )
-          }
+          <BasicMessage book={{text: props.data.social.calendar.text, href: props.data.social.calendar.href}}/>
         </div>
 
         <span className="font-bold inline-block mb-3">Lets Connect on:</span>
---
+++ src/features/basic-message/index.tsx
@@ -0,0 +1,66 @@
+"use client";
+import {SubmitEventHandler, useState} from 'react';
+import {notifyDiscord} from '@/app/actions';
+import {MessageCircle, XIcon} from 'lucide-react';
+import LinkButton from '@/components/lib/link-button';
+import Button from '@/components/lib/button';
+
+interface BasicMessageProps {
+  book: {
+    text: string;
+    href: string;
+  }
+}
+
+export default function BasicMessage(props: BasicMessageProps) {
+
+  const [message, setMessage] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit: SubmitEventHandler = async (event) => {
+    event.preventDefault();
+    if (message.length) {
+      await notifyDiscord(message);
+      setMessage("");
+      setSuccess(true);
+    }
+  };
+
+  return (
+      <>
+
+        <form onSubmit={handleSubmit} className="mt-2">
+          <label className="flex flex-col gap-4 w-full h-full" htmlFor="message">
+            <div className="flex flex-wrap-reverse w-full gap-4 items-baseline justify-between">
+                <span className="flex gap-4 mb-2">
+                <MessageCircle/><span>Write me a message:</span>
+                </span>
+              <LinkButton size="small" className="w-fit" external href={props.book.href}
+                          icon="calendar">
+                {props.book.text}
+              </LinkButton>
+            </div>
+            <textarea className="border border-(--semigrid) p-2 rounded-sm h-30" name="message"
+                      value={message}
+                      onChange={(event) => setMessage(event.currentTarget.value)}
+                      placeholder="Say Hello ..."/>
+            <Button className="self-end" variant="solid-light" type="submit" icon="send"
+                    aspect="square"
+                    disabled={message.length === 0}
+                    iconPosition="right"></Button>
+          </label>
+        </form>
+        {
+            success && (
+                <div
+                    className="relative rounded-sm bg-(--bg-surface-2) p-2 mb-6 flex items-center border border-(--semigrid) w-fit">
+                  Message received. Looking forward to reading it.
+                  <button className="cursor-pointer pl-2" onClick={() => setSuccess(false)}>
+                    <XIcon/>
+                  </button>
+                </div>
+            )
+        }
+      </>
+  )
+}
