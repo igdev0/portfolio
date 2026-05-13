dbfe53615eb48e4abf4a852eaa5d5042ce1c383e
igdev <dorultanianos@gmail.com>
2026-05-13 17:50:41 +0300

    export types explicitly

--- src/schema.ts
+++ src/schema.ts
@@ -2,15 +2,17 @@ import {co} from 'jazz-tools';
 import {Conversation} from '@/features/chat/schema';
 import {z} from 'zod';
 
-export const JazzRoot = co.map({
+export type Root = co.loaded<typeof Root>;
+export const Root = co.map({
   conversations: co.list(Conversation),
 });
 
+export type Account = co.loaded<typeof Account>
 export const Account = co.account({
-  root: JazzRoot,
+  root: Root,
   profile: co.profile({
     avatar: co.image(),
     status: z.enum(['active', 'inactive', 'busy']),
     thought: z.string(),
   }),
-})
+});
