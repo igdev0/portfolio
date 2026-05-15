0375169680685bd6d983c72d6fe9515549747d8c
igdev <dorultanianos@gmail.com>
2026-05-15 13:33:45 +0300

    add banner

--- src/components/lib/collaborate/index.tsx
+++ src/components/lib/collaborate/index.tsx
@@ -7,6 +7,8 @@ import LinkButton from '@/components/lib/link-button';
 import {IconNames} from '@/components/lib/icon';
 import {usePasskeyAuth} from 'jazz-tools/react';
 import Chat from '@/features/chat';
+import Button from '@/components/lib/button';
+import {LockIcon} from 'lucide-react';
 
 interface CollaborateProps {
   data: CollaborateType;
@@ -15,6 +17,7 @@ interface CollaborateProps {
 export default function Collaborate(props: CollaborateProps) {
   const {data} = props;
   const auth = usePasskeyAuth({appName: "IGDev's portfolio"});
+
   return (
       <Container className="pt-40 pb-4" id="collaborate">
         <Comment>
@@ -25,7 +28,11 @@ export default function Collaborate(props: CollaborateProps) {
           {data.statement}
         </Statement>
         <div className="max-w-120 gap-6 w-full">
-          {/*<Button className="mb-2" onClick={auth.logIn}>Log in</Button>*/}
+          {auth.state !== 'signedIn' && <Button className="mb-2" onClick={auth.logIn}>Log in</Button>}
+          <div className="panel gap-2 mb-3">
+            <LockIcon className="text-accent-500"/>
+            <p>This chat is end-to-end encrypted and built using <a className="text-accent-500 underline" href="https://jazz-tools.com">Jazz</a>, local-first architecture.</p>
+          </div>
           <Chat/>
         </div>
 
