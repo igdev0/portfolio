69e56856c4bf587a0062fbcbe263ae355e17fae1
igdev <dorultanianos@gmail.com>
2026-06-04 21:54:52 +0300

    add initial content

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,7 @@ import {useTranslations} from 'next-intl';
 
 export default function Page() {
   const t = useTranslations();
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1>{t('some-kind-of-text')}</h1>
