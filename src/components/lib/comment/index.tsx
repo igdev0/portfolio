88d8cbfe965f5712b570bdb47dc26138c31e7ba7
igdev <dorultanianos@gmail.com>
2026-06-07 11:35:03 +0300

    add while in view for comment component

--- src/components/lib/comment/index.tsx
+++ src/components/lib/comment/index.tsx
@@ -1,20 +1,22 @@
+"use client";
 import {Ref} from 'react';
 import "./index.css";
 import Html from '@/components/lib/html';
 import clsx from 'clsx';
+import {motion} from 'framer-motion';
 
 export type CommentProps = {
   className?: string;
   ref?: Ref<HTMLDivElement>;
   children?: string;
 };
-
+const AnimatedHtml = motion(Html);
 export default function Comment(props: CommentProps) {
   const {children, className = '', ref} = props;
 
   return (
-      <Html className={clsx(`comment ${className}`)} ref={ref}>
+      <AnimatedHtml className={clsx(`comment ${className}`)} ref={ref} whileInView={{opacity: 1, y: 0}} initial={{opacity: 0, y: 10}} viewport={{once: true}}>
         {children}
-      </Html>
+      </AnimatedHtml>
   );
 }
