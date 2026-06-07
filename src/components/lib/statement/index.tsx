fad29831e2fe369539b4f4bee10dc8c4a7c3150c
igdev <dorultanianos@gmail.com>
2026-06-07 12:04:19 +0300

    make statement animation once

--- src/components/lib/statement/index.tsx
+++ src/components/lib/statement/index.tsx
@@ -13,7 +13,7 @@ export default function Statement(props: Props & PropsWithChildren) {
   const {className = '', children, transition} = props;
   return (
       <AnimatedHtml className={clsx('statement', className)} whileInView={{opacity: 1, y: 0}} transition={transition}
-                    initial={{opacity: 0, y: 20}}>
+                    initial={{opacity: 0, y: 20}} viewport={{once: true}}>
         {children}
       </AnimatedHtml>
   );
