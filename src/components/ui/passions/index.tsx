e5513a1ae6edd72ddde1332c1f0bb39697020e83
igdev <dorultanianos@gmail.com>
2026-06-08 17:06:34 +0300

    add parallax

--- src/components/ui/passions/index.tsx
+++ src/components/ui/passions/index.tsx
@@ -4,12 +4,18 @@ import Comment from '@/components/lib/comment';
 import Statement from '@/components/lib/statement';
 import Container from '@/components/lib/container';
 import "./styles.css";
-import {motion} from 'framer-motion';
+import {motion, useScroll, useTransform} from 'framer-motion';
 import Heading from '@/components/lib/heading';
 import Icons from '@/components/lib/icons';
 
 export default function Passions(props: PassionsType) {
   const {title, comment, passions, statement} = props;
+  const {scrollY} = useScroll();
+  const imageY = useTransform(scrollY, [0, 2000], [0, -50]);
+
+  const onMouseMove = () => {
+
+  }
   return (
       <div className="passions" id="passions">
         <Container className="pt-40">
@@ -23,7 +29,14 @@ export default function Passions(props: PassionsType) {
                   <motion.div whileInView={{opacity: 1, y: 0}} initial={{y: 20, opacity: 0}}
                               transition={{delay: index * .1}} viewport={{once: true}}
                               className="passion-card" key={index}>
-                    <div className="card-image" style={{backgroundImage: `url(${passion.image.src})`}}/>
+                    <motion.div
+                      className="card-image"
+                      onMouseMove={onMouseMove}
+                      style={{
+                        backgroundImage: `url(${passion.image.src})`,
+                        backgroundPositionY: imageY,
+                      }}
+                    />
                     <div className="card-content">
                       <h3 className="text-xl flex gap-3 items-center"><Icons name={passion.icon}/> {passion.title}</h3>
                       <p className="bio">{passion.bio}</p>
