e95a96d53bb8a72f857d72b5de2bdad9d0e5d55d
igdev <dorultanianos@gmail.com>
2026-06-13 22:16:54 +0300

    make hero img a fetch priority

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -83,6 +83,7 @@ export default function Hero(props: HeroType) {
                            whileInView={{opacity: 1}}
                            initial={{opacity: 0}}
                            transition={{delay: .3}}
+                           priority
                            viewport={{once: true}}
                            {...image}/>
             <motion.div
