3aa02f0ae24f021f33891dc93e9c38b55d92402e
igdev <dorultanianos@gmail.com>
2026-06-17 09:58:26 +0300

    improve spacing

--- src/components/experience/pin.tsx
+++ src/components/experience/pin.tsx
@@ -22,7 +22,7 @@ export default function ExperiencePin(props: ExperiencePinType) {
           <div className="z-5 sticky top-18 py-3 mb-1 bg-(--background)">
             <h3 className="text-2xl font-bold mb-0">{props.title}</h3>
           </div>
-          <div className="flex flex-wrap gap-4 relative">
+          <div className="flex flex-wrap gap-3 relative">
             <Tag>
               <Building className="stroke-accent-500"/>
               {props.company}
@@ -40,7 +40,7 @@ export default function ExperiencePin(props: ExperiencePinType) {
               {props.location}
             </Tag>
           </div>
-          <Panel className="mt-6">
+          <Panel className="mt-3">
             {props.summary}
           </Panel>
           <ul className="experience-contributions my-4">
@@ -50,7 +50,7 @@ export default function ExperiencePin(props: ExperiencePinType) {
               ))
             }
           </ul>
-          <div className="flex flex-wrap gap-3 mt-5 mb-0">
+          <div className="flex flex-wrap gap-3 mt-6 mb-0">
             {
               props.links.map((link, index) => (
                   <LinkButton key={index}
