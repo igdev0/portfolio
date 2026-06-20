9d610240b090b580ec9ec1ddd1b1a2567897cc50
igdev <dorultanianos@gmail.com>
2026-06-20 23:39:30 +0300

    atempt to fix prod issue

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from "next";
 import {withPayload} from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   allowedDevOrigins: ['192.168.0.234', '192.168.0.234'],
 };
 
