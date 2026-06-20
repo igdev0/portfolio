28065bc7568344944df9f9602fb77592e649dd69
igdev <dorultanianos@gmail.com>
2026-06-20 23:29:26 +0300

    update pnpm & next conf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from "next";
 import {withPayload} from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ['192.168.0.234', '192.168.0.234'],
 };
 
