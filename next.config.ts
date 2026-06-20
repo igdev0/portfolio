9c819cbeb0561303cc25d86437fc834fc73f0179
igdev <dorultanianos@gmail.com>
2026-06-20 23:51:34 +0300

    config next

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import {withPayload} from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.234', '192.168.0.234'],
+  serverExternalPackages: ['sharp', '@img/sharp-linux-x64'],
 };
 
 export default withPayload(nextConfig);
