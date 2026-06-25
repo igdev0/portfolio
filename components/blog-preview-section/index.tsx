2f7c23844296d110014019aee3e268ae11875e8e
igdev <dorultanianos@gmail.com>
2026-06-25 20:03:39 +0300

    move from graphql

--- components/blog-preview-section/index.tsx
+++ components/blog-preview-section/index.tsx
@@ -1,19 +1,18 @@
 import Section from '@/components/section';
 import {SectionType} from '@/content/types';
-import {Blog} from '@/remote/client/gql-generated';
 import BlogPreview from '@/components/blog-preview';
+import {PaginatedDocs} from 'payload';
+import {Blog} from '@/payload-types';
 
-
-export default function BlogPreviewSection(props: SectionType & { blogs: Blog[] }) {
+export default function BlogPreviewSection(props: SectionType & { blogs: PaginatedDocs<Blog> }) {
 
   return (
       <Section title={props.title} comment={props.comment} statement={props.statement}>
         <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
           {
-            props.blogs.map((blog, index) => (
+            props.blogs.docs.map((blog, index) => (
                 <BlogPreview key={index}
-                             slug={blog.slug as string}
-                             {...blog.preview}/>
+                             {...blog}/>
             ))
           }
         </div>
