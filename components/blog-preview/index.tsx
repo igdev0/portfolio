32f3d49fb2a1e48211c01be35326f8de16222848
igdev <dorultanianos@gmail.com>
2026-06-25 20:04:09 +0300

    fix image

--- components/blog-preview/index.tsx
+++ components/blog-preview/index.tsx
@@ -1,21 +1,25 @@
 import Image from 'next/image';
 import "./index.css";
 import Link from 'next/dist/client/link';
-import {Blog_Preview} from '@/remote/client/gql-generated';
+import {Blog, Media} from '@/payload-types';
 
-export default function BlogPreview(props: Blog_Preview & {slug: string}) {
+export default function BlogPreview(props: Blog) {
   return (
       <Link href={`/blog/${props.slug}`} className="blog-preview">
         <div className="flex flex-wrap gap-1">
 
         </div>
         <div className="preview-media">
-          <Image src={`${process.env.NEXT_PUBLIC_MEDIA_STORAGE_URL}/${props.image?.filename}`}
-                 alt={props.image?.alt??"Blog image preview"} width={400} height={200}/>
+          {
+              props.preview?.image && (
+                  <Image src={`${process.env.NEXT_PUBLIC_MEDIA_STORAGE_URL}/${(props.preview?.image as Media)?.filename}`}
+                         alt={(props.preview.image as Media)?.alt ?? "Blog image preview"} width={400} height={200}/>
+              )
+          }
         </div>
         <div className="preview-content">
           <h3 className="text-xl font-medium">{props.title}</h3>
-          <p>{props.description}</p>
+          <p>{props.preview?.description}</p>
         </div>
       </Link>
   );
