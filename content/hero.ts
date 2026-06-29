7424245a5f8cc27a97eb058cdd52165a8f9d68b0
igdev <dorultanianos@gmail.com>
2026-06-29 19:08:19 +0300

    update copy

--- content/hero.ts
+++ content/hero.ts
@@ -3,8 +3,8 @@ import contact from '@/content/contact';
 
 const hero: HeroType = {
   comment: "Ianos G Dorultan",
-  title: "Software Engineer",
-  statement: "Frontend-focused engineer passionate about building modern user experiences.",
+  title: "Frontend Engineer",
+  statement: "Product-focused software engineer turning ideas into fast, scalable, and polished applications.",
   cta0: {
     icon: "chat",
     href: "#message",
