a18db15a75f8f29a6ed29ae8d68bf3eade2e3a20
igdev <dorultanianos@gmail.com>
2026-07-05 14:12:13 +0100

    feat(menu): improve menu accessibility

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -6,9 +6,10 @@ import IconButton from '@/components/icon-button';
 import Container from '@/components/container';
 import {NavType} from '@/content/types';
 import LinkButton from '@/components/link-button';
-import NavMenu from '@/components/nav/menu';
-import {Dispatch, SetStateAction, useRef} from 'react';
 import NavProgress from '@/components/nav/progress';
+import {Menu} from '@base-ui/react';
+import {MenuIcon} from 'lucide-react';
+import clsx from 'clsx';
 
 export interface NavProps extends NavType {
 }
@@ -16,8 +17,6 @@ export interface NavProps extends NavType {
 export default function Nav(props: NavProps) {
   const {theme, setTheme} = useTheme();
   const {brand, links} = props;
-  const menuRef = useRef<{ setMenuOpen: Dispatch<SetStateAction<boolean>> }>(null);
-
   return (
       <nav className="nav">
         <NavProgress/>
@@ -41,10 +40,30 @@ export default function Nav(props: NavProps) {
             <LinkButton label="Github" href={props.github} icon="github" aspect="square" size="xs" variant="ghost"
                         external/>
             <IconButton label="Theme" icon="sun" onClick={() => setTheme(theme === 'dark' ? "light" : "dark")}/>
-            <IconButton label="Menu" className="nav__drawer-toggler" icon="menu"
-                        onClick={() => menuRef.current?.setMenuOpen(true)}></IconButton>
+            <Menu.Root>
+              <Menu.Trigger className="nav__drawer-toggler">
+                <MenuIcon className="stroke-(--foreground)"/>
+              </Menu.Trigger>
+              <Menu.Portal>
+                <Menu.Positioner className="z-20" sideOffset={8}>
+                  <Menu.Popup className="relative bg-(--background) p-3 border border-(--grid) rounded-sm outline-hidden transition-[scale,opacity] duration-100 ease-out data-ending-style:scale-[0.98] data-ending-style:opacity-0 data-starting-style:scale-[0.98] data-starting-style:opacity-0 origin-[var(--transform-origin)]">
+                    {
+                      links.map((entry, index) => (
+                          <Menu.Item key={index} className="py-1">
+                            <Link draggable={false}
+                                  className={clsx(`nav__drawer-link`)}
+                                  href={entry.href}>
+                              {entry.text}
+                            </Link>
+                          </Menu.Item>
+                      ))
+                    }
+                  </Menu.Popup>
+                </Menu.Positioner>
+              </Menu.Portal>
+            </Menu.Root>
           </div>
-          <NavMenu ref={menuRef} {...props}/>
+
         </Container>
       </nav>
   );
--- components/nav/menu.tsx
+++ components/nav/menu.tsx
@@ -1,9 +1,7 @@
 "use client";
 import {Dispatch, Ref, SetStateAction, useImperativeHandle, useState} from 'react';
 import clsx from 'clsx';
-import IconButton from '@/components/icon-button';
 import {NavProps} from '@/components/nav/index';
-import Link from 'next/dist/client/link';
 
 export default function NavMenu(props: NavProps & { ref: Ref<{ setMenuOpen: Dispatch<SetStateAction<boolean>> }> }) {
   const {links} = props;
@@ -16,18 +14,7 @@ export default function NavMenu(props: NavProps & { ref: Ref<{ setMenuOpen: Disp
   }, []);
 
   return (
-
       <div className={clsx('nav__drawer', menuOpen ? 'open' : 'close')}>
-        <IconButton label="Menu Close" icon="x" onClick={() => setMenuOpen(false)}/>
-        {
-          links.map((entry, index) => (
-              <Link draggable={false} key={index}
-                    className={clsx(`nav__drawer-link`, entry.text && "internal", "py-1", "flex justify-end")}
-                    href={entry.href}>
-                {entry.text}
-              </Link>
-          ))
-        }
       </div>
   );
 }
